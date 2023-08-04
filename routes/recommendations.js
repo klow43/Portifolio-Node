14d7cde45cd4d56440622cbd6545254275aeefd5
Karen Beate <karen.beate@outlook.com>
2023-08-04 19:28:04 +0200

    Refactored recommendations

--- routes/recommendations.js
+++ routes/recommendations.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const fs = require("fs");
+const path = require("path");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('recommendations');
+  let data = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"))
+  res.render('recommendations', {data: JSON.parse(data)});
 });
 
 module.exports = router;
