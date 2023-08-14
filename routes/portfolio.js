36ed9bc691798206fc1000b13133bf311cbfbaf6
Karen Beate <karen.beate@outlook.com>
2023-08-14 20:18:45 +0200

    Validate Post Delete

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -20,13 +20,15 @@ router.get('/', function(req, res, next) {
 
 router.post('/',jsonParser, function(req, res, next){
   let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/portfolio.json"));
-  let portfoliosArray = JSON.parse(rawdata);
-  if(portfoliosArray.filter(x => x.name === req.body.name).length == 0){
-    download(req.body.url, req.body.name, function(){
-      console.log('done');
-    });
-    const newArray = portfoliosArray.concat([req.body])
-    fs.writeFileSync(path.resolve(__dirname, "../data/portfolio.json"), JSON.stringify(newArray));
+  let portfoliosArray = JSON.parse(rawdata);  
+  if(validationPOST(req,res,portfoliosArray) == true){
+    if(portfoliosArray.filter(x => x.name === req.body.name).length == 0){
+        download(req.body.url, req.body.name, function(){
+          console.log("Image posted and saved : " + req.body.name)
+        });
+        const newArray = portfoliosArray.concat([req.body])
+        fs.writeFileSync(path.resolve(__dirname, "../data/portfolio.json"), JSON.stringify(newArray));
+      }
   }
   res.end();
 });
@@ -34,14 +36,62 @@ router.post('/',jsonParser, function(req, res, next){
 router.delete('/', jsonParser, function(req, res, next){
   let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/portfolio.json"));
   let portfoliosArray = JSON.parse(rawdata);
-  const newArray = portfoliosArray.filter(x => x.name !== req.body.name)
-  if(newArray.length !== portfoliosArray.length){
+  if(validationDELETE(req,res,portfoliosArray) == true){
+    let newArray = portfoliosArray.filter(x => x.name !== req.body.name)
     fs.unlink(path.resolve(__dirname, '../data/img/' + req.body.name), () => {
-      console.log(req.body.name + "deleted!");
-    });
     fs.writeFileSync(path.resolve(__dirname, "../data/portfolio.json"), JSON.stringify(newArray));
-  }
+    console.log("Image deleted : " + req.body.name)
+    })
+  } 
   res.end();
 })
+
+//validation for POST request
+function validationPOST(req,res){
+  const expectedAttributes = ["url", "name", "alt", "category", "header", "description"]
+    Object.keys(req.body).forEach(param => {
+      if(!(expectedAttributes.includes(param))){
+        res.status(400).end("Attributes incorrect")
+      };
+    })
+    if(req.body.url == null || req.body.url.length === 0){
+      res.status(400).end("URL input not provided");
+    }
+    if(req.body.name == null || req.body.name.length === 0){
+      res.status(400).end("Name input not provided");
+    }
+    if(req.body.alt == null || req.body.name.length === 0){
+      res.status(400).end("Alt not provided");
+    }
+    if(req.body.header == null || req.body.header.length === 0){
+      res.status(400).end("Header not provided");
+    }
+    if(req.body.description == null || req.body.description.length === 0){
+      res.status(400).end("Description not provided");
+    }
+    if(!(["wedding", "christmas", "birthday", "anniversary"].includes(req.body.category))){
+      res.status(400).end("Wrong category provided");
+      } 
+      if(portfoliosArray.find(x => x.name == req.body.name) !== undefined){
+        res.status(400).end("Name already in use")
+      }
+  else{return true}
+}
+
+function validationDELETE(req,res,portfoliosArray){  
+  const expectedAttributes = ["name"]
+  Object.keys(req.body).forEach(param => {
+      if(!(expectedAttributes.includes(param))){
+        res.status(400).end("Attributes incorrect")
+      }
+    });
+  if(req.body.name == null || req.body.name.length === 0){
+      res.status(400).end("Name input not provided");
+      }
+  if(portfoliosArray.find(x => x.name == req.body.name) === undefined){
+    res.status(400).end("Name not in registery")
+  }
+  else{return true}
+}
 
 module.exports = router;
--- routes/recommendations.js
+++ routes/recommendations.js
@@ -1,34 +1,74 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const fs = require("fs");
 const path = require("path");
-var bodyParser = require('body-parser');
-var jsonParser = bodyParser.json();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
-/* GET home page. */
+/* GET recommendations page. */
 router.get('/', function(req, res, next) {
   let data = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"))
   res.render('recommendations', {data: JSON.parse(data)});
 });
 
 router.post('/', jsonParser, function(req, res, next) {
-  let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
-  let recommendationsArray = JSON.parse(rawdata);
+let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
+let recommendationsArray = JSON.parse(rawdata);  
+if(validationPOST(req,res) == true){  
   if(recommendationsArray.filter(x => x.name === req.body.name).length == 0) {
     const newArray = recommendationsArray.concat([req.body])
-    fs.writeFileSync(path.resolve(__dirname, "../data/recommendations.json"), JSON.stringify(newArray));
-  }
-  res.end();
+    fs.writeFileSync(path.resolve(__dirname, "../data/recommendations.json"), JSON.stringify(newArray));  
+      };
+    };
+  res.end() 
 });
 
 router.delete('/', jsonParser, function(req,res,next){
   let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
   let recommendationsArray = JSON.parse(rawdata);
   const newArray = recommendationsArray.filter(x => x.name !== req.body.name)
-  if(newArray.lenght !== recommendationsArray.length) {
-    fs.writeFileSync(path.resolve(__dirname, "../data/recommendations.json"), JSON.stringify(newArray));
-  }
+  if(validationDELETE(req,res) == true){
+      if(newArray.lenght !== recommendationsArray.length) {
+      fs.writeFileSync(path.resolve(__dirname, "../data/recommendations.json"), JSON.stringify(newArray));
+      };
+    };
   res.end();
 });
+
+//validate POST input
+function validationPOST(req,res){
+  const expectedAttribute = ["avatar", "name", "role", "description"]
+ //checking if wrong keys in request
+  Object.keys(req.body).forEach(param => {
+    if(!(expectedAttribute.includes(param))){
+      res.status(400).end("Wrong atttributes" + param);
+    }
+  });
+    //name is null or empty
+    if(req.body.name == null || req.body.name.length === 0){
+      res.status(400).end("Name not provided");
+    }
+    //role is null or empty
+    if(req.body.role == null || req.body.role.length === 0  ){
+      res.status(400).end("Role not provided");
+    }
+    //description null or empty
+    if(req.body.description == null || req.body.description.length === 0){
+      res.status(400).end("Description not provided");
+    }
+    //avatar not included in body or contains off limit number.
+    if(!([1,2,3].includes(req.body.avatar))){
+      res.status(400).end("Wrong/No avatar provided");
+    }
+  else{return true}
+}
+
+//validation for DELETE request
+function validationDELETE(req,res){
+  if(req.body.name == null || req.body.name.length === 0){
+    res.status(400).end("Name not provided");
+  }
+  else{return true}
+}
 
 module.exports = router;
